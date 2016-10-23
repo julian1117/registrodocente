608f90dd05160fd76028d7bd7642693a4eb875bc
caferrerb <caferrerb@eam.edu.co>
2016-10-23 16:09:42 -0500

    namedquieries

---
+++ registrodocentes/registrodocentes-ejb/src/main/java/co/edu/eam/ingesoft/pa2/registrodocentes/util/ConstantesNamedQueries.java
@@ -0,0 +1,22 @@
+package co.edu.eam.ingesoft.pa2.registrodocentes.util;
+
+/**
+ * 
+ * Clase responsable de alojar los nombres de las NAMEDQUeries <br>
+ * 
+ * @author EAM <br>
+ *         Camilo Andres Ferrer Bustos<br>
+ * 
+ * 
+ * @date 19/10/2016
+ * @version 1.0
+ */
+public class ConstantesNamedQueries {
+	
+	//public static final String CONSULTA.....;
+	
+	private ConstantesNamedQueries() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
