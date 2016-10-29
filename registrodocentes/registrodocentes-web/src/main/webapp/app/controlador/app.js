351e63b9b33f2acb515ea6cffbabe28db1aed5ad
Sergio <serginhosacf@gmail.com>
2016-10-29 15:01:33 -0500

    sergio

--- registrodocentes/registrodocentes-web/src/main/webapp/app/controlador/app.js
+++ registrodocentes/registrodocentes-web/src/main/webapp/app/controlador/app.js
@@ -31,6 +31,11 @@ app.config(function($routeProvider){
             controller: "inventariocontroller",
             
             templateUrl: "vistas/page3.html"
-        });
+        })
+    	.when("/programasDocentes", {
+        controller: "inventariocontroller",
+        
+        templateUrl: "vistas/programasDocentes.html"
+    });
 });
 
