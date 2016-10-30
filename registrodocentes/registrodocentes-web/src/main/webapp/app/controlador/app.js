dc83a7a013baf5431e7bfc8ea3f0e249d18ae89d
Jairo Andres <jairo199316@hotmail.com>
2016-10-29 20:15:44 -0500

    Botones en el menu y direcciones en el app

--- registrodocentes/registrodocentes-web/src/main/webapp/app/controlador/app.js
+++ registrodocentes/registrodocentes-web/src/main/webapp/app/controlador/app.js
@@ -1,63 +1,47 @@
-var app = angular.module("miApp",["ngRoute","ngStorage"]);
+var app = angular.module("miApp", [ "ngRoute", "ngStorage" ]);
 
-app.config(function($routeProvider){
-    $routeProvider
-        .when("/", {
-            controller: "modulo1Controller",
-            controllerAs: "m1",
-            templateUrl: "vistas/form.html"
-        })
-        .when("/registros", {
-            controller: "registrosController",
-            controllerAs: "reg",
-            templateUrl: "vistas/registros.html"
-        })
-        .when("/aprobar-registro", {
-            controller: "aprobarRegistroController",
-            controllerAs: "apr",
-            templateUrl: "vistas/aprobar-registro.html"
-        })
-        .when("/AsignarAcceso", {
-            controller: "modulo1Controller",
-            controllerAs: "tra",
-            templateUrl: "vistas/AsignarAcceso.html"
-        })
-        .when("/login", {
-            controller: "modulo1Controller",
-            controllerAs: "m1",
-            templateUrl: "vistas/login.html"
-        })
-        .when("/cliente", {
-            controller: "clientecontroller",
-            controllerAs: "cliente",
-            templateUrl: "vistas/cliente.html"
-        })
-        .when("/RegSemAnteriores", {
-            controller: "inventariocontroller",
-            
-            templateUrl: "vistas/RegSemAnteriores.html"
-        })
-	    .when("/reporteregistro", {
-	        controller: "inventariocontroller",
-	        
-	        templateUrl: "vistas/ReporteRegistros.html"
-	    })
-    	.when("/programasDocentes", {
-        controller: "inventariocontroller",
-        
-        templateUrl: "vistas/programasDocentes.html"
-    	})
-        .when("/CrearRol", {
-            controller: "inventariocontroller",
-            
-            templateUrl: "vistas/CrearRol.html"
-        })
-        
-        .when("/RegSemAnteriores", {
-            controller: "regSemAnteriores",
-            
-            templateUrl: "vistas/RegSemAnteriores.html"
-        })
-        ;
+app.config(function($routeProvider) {
+	$routeProvider.when("/", {
+		controller : "modulo1Controller",
+		controllerAs : "m1",
+		templateUrl : "vistas/form.html"
+	}).when("/registros", {
+		controller : "registrosController",
+		controllerAs : "reg",
+		templateUrl : "vistas/registros.html"
+	}).when("/aprobar-registro", {
+		controller : "aprobarRegistroController",
+		controllerAs : "apr",
+		templateUrl : "vistas/aprobar-registro.html"
+	}).when("/AsignarAcceso", {
+		controller : "modulo1Controller",
+		controllerAs : "tra",
+		templateUrl : "vistas/AsignarAcceso.html"
+	}).when("/login", {
+		controller : "modulo1Controller",
+		controllerAs : "m1",
+		templateUrl : "vistas/login.html"
+	}).when("/cliente", {
+		controller : "clientecontroller",
+		controllerAs : "cliente",
+		templateUrl : "vistas/cliente.html"
+	}).when("/RegSemAnteriores", {
+		controller : "inventariocontroller",
+		templateUrl : "vistas/RegSemAnteriores.html"
+	}).when("/ReporteRegistros", {
+		controller : "inventariocontroller",
+		templateUrl : "vistas/ReporteRegistros.html"
+	}).when("/programasDocentes", {
+		controller : "inventariocontroller",
+		templateUrl : "vistas/programasDocentes.html"
+	}).when("/asignaturas", {
+		controller : "inventariocontroller",
+		templateUrl : "vistas/asignaturas.html"
+	}).when("/Semestre", {
+		controller : "inventariocontroller",
+		templateUrl : "vistas/Semestre.html"
+	}).when("/CrearRol", {
+		controller : "inventariocontroller",
+		templateUrl : "vistas/CrearRol.html"
+	});
 });
-
