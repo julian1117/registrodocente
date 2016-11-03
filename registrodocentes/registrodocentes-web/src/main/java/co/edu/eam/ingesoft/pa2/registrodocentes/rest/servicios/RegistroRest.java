8a31206f419a3f38193b32602423f84ab38fb121
Brian David Tafur Londoño <briantafur89@hotmail.com>
2016-11-03 09:51:28 -0500

    Servicio para listar los registros realizados por un docente, 8 días antes y después de la fecha actual.

--- registrodocentes/registrodocentes-web/src/main/java/co/edu/eam/ingesoft/pa2/registrodocentes/rest/servicios/RegistroRest.java
+++ registrodocentes/registrodocentes-web/src/main/java/co/edu/eam/ingesoft/pa2/registrodocentes/rest/servicios/RegistroRest.java
@@ -1,8 +1,12 @@
 package co.edu.eam.ingesoft.pa2.registrodocentes.rest.servicios;
 
 import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import co.edu.eam.ingesoft.pa2.registrodocentes.bo.BORegistroEJB;
@@ -24,6 +28,15 @@ public class RegistroRest {
 	@javax.ws.rs.Produces(MediaType.APPLICATION_JSON)
 	public RespuestaDTO listarRegistros(){
 		return registroEJB.listarRegistros();
+	}
+	
+	@POST
+	@Path("/listar-registros-fechas")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.APPLICATION_JSON)
+	public RespuestaDTO listarRegistrosEntreFechas(@FormParam(value = "cod")String cod, @FormParam(value = "ced") int ced){
+		RespuestaDTO res = new RespuestaDTO(registroEJB.listarRegistroEntreFechas(cod, ced));
+		return res;
 	}
 	
 	
