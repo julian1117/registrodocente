ba415fd51da21c8b086c556fce3d02a50915fb44
Jhohanns <jhohannss@hotmail.com>
2016-11-05 14:24:39 -0500

    FILTRO APLICADO

--- registrodocentes/registrodocentes-web/src/main/java/co/edu/eam/ingesoft/pa2/registrodocentes/rest/util/FiltroSeguridadRest.java
+++ registrodocentes/registrodocentes-web/src/main/java/co/edu/eam/ingesoft/pa2/registrodocentes/rest/util/FiltroSeguridadRest.java
@@ -16,25 +16,25 @@ import co.edu.eam.ingesoft.pa2.registrodocentes.rest.servicios.LoginRest;
 //@Secured//indicar al filtro que se interceptara lo anotado con esto
 //@Provider
 public class FiltroSeguridadRest implements ContainerRequestFilter {
-	@EJB
-	AccesoEJB acc;
+//	@EJB
+//	AccesoEJB acc;
 
 	@Override
 	public void filter(ContainerRequestContext ctxReq) throws IOException {
 		String token=ctxReq.getHeaderString("Authorization");
 		
-		List<String> meto = ctxReq.getUriInfo().getMatchedURIs();
-		String metod = meto.get(0);
+//		List<String> meto = ctxReq.getUriInfo().getMatchedURIs();
+//		String metod = meto.get(0);
 		//TODO: revisar el usuario del token y el permiso de acceso al servicio
 		if(!LoginRest.tokens.containsKey(token)){
 			RespuestaDTO dto = new RespuestaDTO(null, "No Autorizado", "-3");
 			Response res=Response.status(401).entity(dto).build();
 			ctxReq.abortWith(res);
 		}
-		String usere =(String)LoginRest.tokens.get(token);
-		if(meto!=null){
-			
-		}
+//		String usere =(String)LoginRest.tokens.get(token);
+//		if(meto!=null){
+//			
+//		}
 		
 	}
 
