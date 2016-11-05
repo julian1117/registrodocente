aa4679016e0922195aa647203f07d72246207add
Brian David Tafur Londoño <briantafur89@hotmail.com>
2016-11-05 08:23:48 -0500

    Implementación ventana registros, bloqueo a registros aprobados

--- registrodocentes/registrodocentes-web/src/main/webapp/app/controlador/registrosController.js
+++ registrodocentes/registrodocentes-web/src/main/webapp/app/controlador/registrosController.js
@@ -3,5 +3,50 @@
  */
 app.controller("registrosController", function($scope, $http, httpservice,
 		$sessionStorage) {
-	
+
+	$scope.registros = [];
+
+	/**
+	 * Función para cargar los registros
+	 */
+	function cargarRegistros() {
+		var xsrf = $.param({
+			cod : 1,
+			ced : 123
+		});
+		console.log(xsrf);
+		$http(
+				{
+					url : '../rest/registro/listarregistrosfechas',
+					method : "POST",
+
+					data : xsrf,
+					headers : {
+						"Content-Type" : "application/x-www-form-urlencoded"//,
+						//"Authorization" : $sessionStorage.usuario.token
+					}
+
+				}).success(
+				function(data, status, headers, config) {
+					if (data.codigo == '00') {
+						$scope.registros = data.obj;
+					} else {
+						alert(data.mensaje);
+					}
+
+				}).error(
+				function(data, status, headers, config) {
+					alert('error::' + data.mensaje);
+				});
+	}
+
+	/**
+	 * funcion de inicializacion
+	 */
+	(function() {
+		console.log('llegué al controlador');
+		cargarRegistros();
+
+	}());
+
 });
