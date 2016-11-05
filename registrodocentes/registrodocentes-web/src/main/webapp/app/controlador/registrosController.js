a66bd07156ba43784fcdb56c600f729f36c4d981
Brian David Tafur Londoño <briantafur89@hotmail.com>
2016-11-05 13:06:20 -0500

    import

--- registrodocentes/registrodocentes-web/src/main/webapp/app/controlador/registrosController.js
+++ registrodocentes/registrodocentes-web/src/main/webapp/app/controlador/registrosController.js
@@ -10,6 +10,8 @@ app.controller("registrosController", function($scope, $http, httpservice,
 	 * Función para cargar los registros
 	 */
 	function cargarRegistros() {
+		//Se cargan los registros del curso y docente indicados
+		//Deben ser reemplazados por los valores del sessionstorage
 		var xsrf = $.param({
 			cod : 1,
 			ced : 123
