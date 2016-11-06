7e27d5ae24a40a00f7225a6879e99852877969e5
Alejandro <Alejandro@Alejandro>
2016-11-05 19:18:17 -0500

    Tabla Registro de docentes

--- registrodocentes/registrodocentes-web/src/main/webapp/app/controlador/semestresAnterioresRegController.js
+++ registrodocentes/registrodocentes-web/src/main/webapp/app/controlador/semestresAnterioresRegController.js
@@ -3,7 +3,8 @@ app.controller(
 				function($scope,$http , httpservice,$sessionStorage) {
 
 					$scope.usuarios = [];
-					$scope.cursos = [];				
+					$scope.cursos = [];		
+					$scope.registros = [];
 					
 					
 					function listarUsuarios(){
@@ -29,7 +30,6 @@ app.controller(
 					
 					$scope.listarCurso=function(){
 						
-						alert($scope.anio + 'locota'+ $scope.periodo + $scope.docente)
 						var parametro = $.param({
 							anio : $scope.anio,
 							periodo : $scope.periodo,
@@ -60,5 +60,46 @@ app.controller(
 						
 					}
 					
+					
+//					$scope.listarRegistros=function(){
+//						
+//						alert( $scope.docente)
+//						var parametro = $.param({
+//							docente : $scope.docente
+//							
+//						});
+//
+//						$http({
+//							url: '../rest/semestresAnteriores/listarRegistros',
+//						    method: "POST",
+//						    data : parametro,
+//						    headers: {
+//						    		"Content-type" : "application/x-www-form-urlencoded"
+//						    }
+//						    
+//						}).success(function(data, status, headers, config) {
+//							
+//							if(data.codigo=='00'){
+//								$scope.registros = data.obj;
+//								alert('El Curso con estos datos es: '+anio+periodo+docente);
+//							}else{
+//								alert(data.mensaje);
+//							}
+//							
+//						}).error(function(data, status, headers, config) {
+//							alert('Error:'+data.mensaje);
+//						});
+//						
+//					}
+					
+					
+					$scope.listarRegistros=function(docente){			
+						console.log('refrescando.......'+docente);
+						httpservice.post('semestresAnteriores/listarRegistros',
+								{docente:docente},success = function(data, status, headers,config) {
+									console.log('success.......');
+									$scope.registros = data.obj;
+								}, null,"application/x-www-form-urlencoded");											
+					}
 					
 				});
