9cba065f3b8e2b42c74a37e58bd4d82608ea68d3
Alejandro <Alejandro@Alejandro>
2016-11-05 19:23:05 -0500

    Nueva tabla del registro de un curso

--- registrodocentes/registrodocentes-web/src/main/webapp/app/controlador/semestresAnterioresRegController.js
+++ registrodocentes/registrodocentes-web/src/main/webapp/app/controlador/semestresAnterioresRegController.js
@@ -1,108 +1,80 @@
-app.controller(
-				"semestresAnterioresRegController",
-				function($scope,$http , httpservice,$sessionStorage,$window,$location) {
-					 var res = filtrod($window,$location);
-						if (res == true) {
+app.controller("semestresAnterioresRegController", function($scope, $http,
+		httpservice, $sessionStorage, $window, $location) {
+	var res = filtrod($window, $location);
+	if (res == true) {
 
-					$scope.usuarios = [];
-					$scope.cursos = [];		
-					$scope.registros = [];
-					
-					
-					function listarUsuarios(){
-						console.log('refrescando.......');
-						httpservice.get('semestresAnteriores/listaUsuarios', null,
-								success = function(data, status, headers,
-										config) {
-									console.log('success.......');
-									$scope.usuarios = data.obj;
-								}, null);
-					}
-					
-					
-					
-					/**
-					 * funcion de inicializacion
-					 */
-					(function() {						
-						listarUsuarios();
-					}());
-					
-					
-					
-					$scope.listarCurso=function(){
-						
-						var parametro = $.param({
-							anio : $scope.anio,
-							periodo : $scope.periodo,
-							docente : $scope.docente
-							
-						});
+		$scope.usuarios = [];
+		$scope.cursos = [];
+		$scope.registros = [];
 
-						$http({
-							url: '../rest/semestresAnteriores/listarCursos',
-						    method: "POST",
-						    data : parametro,
-						    headers: {
-						    		"Content-type" : "application/x-www-form-urlencoded"
-						    }
-						    
-						}).success(function(data, status, headers, config) {
-							
-							if(data.codigo=='00'){
-								$scope.cursos = data.obj;
-								alert('El Curso con estos datos es: '+anio+periodo+docente);
-							}else{
-								alert(data.mensaje);
-							}
-							
-						}).error(function(data, status, headers, config) {
-							alert('Error:'+data.mensaje);
-						});
-						
-					}
+		/**
+		 * Funcion para listar los usuarios
+		 */
+		function listarUsuarios() {
+			console.log('refrescando.......');
+			httpservice.get('semestresAnteriores/listaUsuarios', null,
+					success = function(data, status, headers, config) {
+						console.log('success.......');
+						$scope.usuarios = data.obj;
+					}, null);
+		}
+
+		/**
+		 * funcion de inicializacion
+		 */
+		(function() {
+			listarUsuarios();
+		}());
+
+		/**
+		 * Funcion para listar los cursos de un profesor con datos espesificos
+		 */
+		$scope.listarCurso = function() {
+
+			var parametro = $.param({
+				anio : $scope.anio,
+				periodo : $scope.periodo,
+				docente : $scope.docente
+
+			});
+
+			$http({
+				url : '../rest/semestresAnteriores/listarCursos',
+				method : "POST",
+				data : parametro,
+				headers : {
+					"Content-type" : "application/x-www-form-urlencoded"
+				}
+
+			}).success(
+					function(data, status, headers, config) {
+
+						if (data.codigo == '00') {
+							$scope.cursos = data.obj;
+							alert('El Curso con estos datos es: ' + anio
+									+ periodo + docente);
+						} else {
+							alert(data.mensaje);
 						}
-					
-					
-//					$scope.listarRegistros=function(){
-//						
-//						alert( $scope.docente)
-//						var parametro = $.param({
-//							docente : $scope.docente
-//							
-//						});
-//
-//						$http({
-//							url: '../rest/semestresAnteriores/listarRegistros',
-//						    method: "POST",
-//						    data : parametro,
-//						    headers: {
-//						    		"Content-type" : "application/x-www-form-urlencoded"
-//						    }
-//						    
-//						}).success(function(data, status, headers, config) {
-//							
-//							if(data.codigo=='00'){
-//								$scope.registros = data.obj;
-//								alert('El Curso con estos datos es: '+anio+periodo+docente);
-//							}else{
-//								alert(data.mensaje);
-//							}
-//							
-//						}).error(function(data, status, headers, config) {
-//							alert('Error:'+data.mensaje);
-//						});
-//						
-//					}
-					
-					
-					$scope.listarRegistros=function(docente){			
-						console.log('refrescando.......'+docente);
-						httpservice.post('semestresAnteriores/listarRegistros',
-								{docente:docente},success = function(data, status, headers,config) {
-									console.log('success.......');
-									$scope.registros = data.obj;
-								}, null,"application/x-www-form-urlencoded");											
-					}
-					
-				});
+
+					}).error(function(data, status, headers, config) {
+				alert('Error:' + data.mensaje);
+			});
+
+		}
+	}
+	
+	/**
+	 * Funcion para listar los registros de los docentes
+	 */
+	$scope.listarRegistros = function(docente) {
+		console.log('refrescando.......' + docente);
+		httpservice.post('semestresAnteriores/listarRegistros', {
+			docente : docente
+		}, success = function(data, status, headers, config) {
+			console.log('success.......');
+			$scope.registros = data.obj;
+		}, null, "application/x-www-form-urlencoded");
+	}
+
+});
